786cd17977c39a477844cd12b58e5048ee4f3cf9
otmane <maacha98otmane@gmail.com>
2021-12-23 15:53:36 +0100

    Process Admin General

--- controller/admin_genaral/auth.js
+++ controller/admin_genaral/auth.js
@@ -1,11 +1,65 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const {
-    promisify
-} = require('util')
-const {
     db
 } = require('../../db/index')
+////////////////////
+exports.login = async (req, res) => {
+    try {
+        const {
+            email,
+            password
+        } = req.body
+        console.log(req.body)
+        if (!email || !password) {
+            return res.status(401).send({
+                msg: "Please add an email and password"
+            })
+        }
+        db.query('select * from admin_genirale where email = ?', [email], async (err, result) => {
+            if (!result || !(await bcrypt.compare(password, result[0].password))) {
+                return res.status(401).send({
+                    message: 'email or password is incorrect'
+                })
+            } else {
+                const id = result[0].id;
+                const token = jwt.sign({
+                    id
+                }, process.env.JWT_SECRET, {
+                    expiresIn: process.env.JWT_EXPIRE_IN
+                })
+                // const cookieOptions = {
+                //     expires: new Date(
+                //         Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+                //     ),
+                //     httpOnly: true
+                // }
+                return res.status(200).send({
+                    msg: "lOGIN SUCCES",
+                    token: token
+                })
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+exports.isLoginIn = async (req, res, next) => {
+
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+        req.userData = decoded;
+        return next()
+    } catch (err) {
+        return res.status(401).send({
+            msg: 'Your session is not valid!'
+        });
+    }
+}
 exports.creation = (req, res) => {
     const {
         name,
@@ -21,12 +75,12 @@ exports.creation = (req, res) => {
         }
 
         if (result.length > 0) {
-            return res.render('/admin_general/creation', {
-                message: 'that email is already in use'
+            return res.status(401).send({
+                msg: "email as ready used"
             })
         } else if (password !== passwordconfirm) {
-            return res.render('/admin_general/creation', {
-                message: 'password do not match'
+            return res.status(401).send({
+                msg: "Password do not match"
             })
         }
         let hashedpassword = await bcrypt.hash(password, 8)
@@ -42,8 +96,8 @@ exports.creation = (req, res) => {
                 console.log(err)
             } else {
                 console.log(result)
-                return res.render('/admin_general/creation', {
-                    message: 'Admin center Created'
+                return res.status(200).send({
+                    msg: "Add admin center"
                 })
             }
         })
@@ -57,94 +111,71 @@ exports.creationcentre = (req, res) => {
         if (err) {
             console.log(err)
         }
-
         if (result.length > 0) {
-            return res.render('admin_general/creationcentre', {
-                message: 'that name is already in use'
+            return res.status(401).send({
+                msg: "name already used"
             })
         }
         db.query('insert into center set ?', {
             name: name,
         }, (err, result) => {
             if (err) {
-                console.log(err)
-            } else {
-                db.query('select * from center', (err, result) => {
-                    return res.render('admin_general/creation', {
-                        req: result
-                    })
+                return res.status(401).send({
+                    msg: err
                 })
-
+            } else {
+                return res.status(200).send({
+                    msg: "Center Created"
+                })
             }
         })
     })
 }
-exports.login = async (req, res) => {
-    try {
-        const {
-            email,
-            password
-        } = req.body
-        if (!email || !password) {
-            return res.status(400).render('login', {
-                message: 'Please add an email and password'
+exports.update = (req, res) => {
+
+    const {
+        name,
+        prenom,
+        email,
+    } = req.body
+    const {
+        id,
+    } = req.params
+    db.query('update admin_center set nom = ?,prenom= ?,email= ? where id=?', [name, prenom, email, id], (err, result) => {
+        if (err) {
+            return res.status(401).send({
+                msg: err
+            })
+        } else {
+            return res.status(200).send({
+                msg: "Update admin center"
             })
         }
-        db.query('select * from admin_genirale where email = ?', [email], async (err, result) => {
-            if (!result || !(await bcrypt.compare(password, result[0].password))) {
-                return res.status(401).render('login', {
-                    message: 'email or password is incorrect'
-                })
-            } else {
-                const id = result[0].id;
-                const token = jwt.sign({
-                    id
-                }, process.env.JWT_SECRET, {
-                    expiresIn: process.env.JWT_EXPIRE_IN
-                })
-                console.log("token" + token)
-                const cookieOptions = {
-                    expires: new Date(
-                        Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-                    ),
-                    httpOnly: true
-                }
-                res.cookie('jwt', token, cookieOptions)
-                res.status(200).redirect("/generaladmin/profile")
-            }
-        })
-    } catch (error) {
-        console.log(error)
-    }
+    })
 }
-exports.isLoginIn = async (req, res, next) => {
-    console.log(req.cookies)
-    if (req.cookies.jwt) {
-        try {
-            const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET)
-            console.log(decoded)
-            db.query('select * from admin_genirale where id = ?', [decoded.id], (error, result) => {
-                console.log(result)
-                if (!result) {
-                    return next()
-                }
-                req.user = result[0]
-                return next()
+exports.delete = (req, res) => {
+    const {
+        id,
+    } = req.params
+    db.query('delete from admin_center where id=?', id, (err, result) => {
+        if (err) {
+            return res.status(401).send({
+                msg: err
             })
-        } catch (error) {
-            console.log(error)
-            return next()
+        } else {
+            return res.status(200).send({
+                msg: "admin center delete"
+            })
         }
-    } else {
-        next();
+    })
 
-    }
 }
-
 exports.logout = async (req, res) => {
     res.cookie('jwt', 'logout', {
         expires: new Date(Date.now() + 2 * 1000),
         httpOnly: true
     })
-    res.status(200).redirect('/generaladmin/login')
+    return res.status(200).send({
+        msg: "Logout"
+    })
 }
